3cddc5be43c770752fa20fa5a9eb34a99821095a
Ömer Faruk Kavak <kvkomerfaruk@gmail.com>
2022-10-01 02:13:23 +0300

    player movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    
+        if (Input.GetKey(KeyCode.W))
+            body.velocity = new Vector2(body.velocity.x, speed * 2);
     }
 }
